51dd01dbb9bdb25fb3ca43d2fbc1cac931fbb0f4
Cuong <cuongvip1295@gmail.com>
2018-03-03 14:18:19 +0700

    hotel admin version 3

---
+++ src/main/java/statics/helper/MathCalculator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statics.helper;
+
+/**
+ *
+ * @author HUNGCUONG
+ */
+public class MathCalculator {
+	
+	public static double round(double value) {
+		return round(value,2);
+	}
+
+    public static double round(double value, int places) {
+        if (places < 0) {
+            throw new IllegalArgumentException();
+        }
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+}
